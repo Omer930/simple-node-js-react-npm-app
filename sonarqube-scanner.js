8b551ac652a0b171bc3303df35fe8272e051a3e6
Omer930 <88266036+Omer930@users.noreply.github.com>
2021-08-21 06:45:22 +0000

    Update sonarqube-scanner.js

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -2,7 +2,7 @@ const scanner = require('sonarqube-scanner');
 scanner(
     {
         serverUrl: "http://localhost:9000",
-        token: "ea4142d52b5563b4814af361169669e408cc2ba5",
+        token: "40e418c28550cfa657d7f2931f45d3773d5af722",
         options: {
             "sonar.sources": "./src"
         },
